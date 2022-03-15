efb14c2852d4665086ac62ca4013f2643184b72e
SouthpawCurveConcepts <jagoblacksky@gmail.com>
2022-03-15 12:55:34 -0500

    functional component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Variables from "./Components/variables.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>React ES6 Variables</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/SouthpawCurveConcepts/rsb-es6-variables"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Link to repo
         </a>
+        <Variables />
       </header>
     </div>
   );
---
+++ src/Components/variables.js
@@ -0,0 +1,5 @@
+function Variables() {
+  return <h2>This is a functional class component for Variables</h2>;
+}
+
+export default Variables;
